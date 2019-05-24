5b483fa66407ac1479a52e602a767e517422115b
Karsten Veltman <karsten.veltman@coderclass.nl>
2019-05-24 13:02:58 +0200

    grid whooo

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+var gridx = 200;
+var gridy = 400;
+
+
 function setup(){
-  createCanvas(1500, 830);
+  createCanvas(400, 800);
 }
 
 function draw(){
-  background('grey');
-  strokeWeight(4);
-  stroke(500);
-  rect(550, 20, 400, 800);
-  rect(20, 70, 500, 250)
-  color("black")
-  text("HOLD", 25, 0, 40);
+  background(220);
+	//For (var BEGIN; END; INTERVAL){
+	//DO SOMETHING }
+	for (var x = 0; x < gridx; x += gridx / 10) {
+    for (var y = 0; y < gridy; y += gridy / 20) {
+      fill(255)
+      rect(x, y, gridx/10, gridy/20)
+      rect(x, y, gridx/10, gridy/20)
+
+		}
+	}
 }
